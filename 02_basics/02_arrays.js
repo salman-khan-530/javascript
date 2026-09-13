6d3bf2355d311b9e84d1b31c897844f51d1a96c8
salman-khan-530 <salman22000055@gmail.com>
2026-09-13 23:06:08 +0500

    More About Array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,22 @@
+const myCities = ["charsadda", "Mardan", "Peshawar"]
+const myCountries = ["Pakistan", "India", "England"]
+
+// myCities.push(myCountries)  
+
+// console.log(myCities);
+// console.log(myCities[3][0]);
+
+
+// const all_cities_and_countries = myCities.concat(myCountries)
+// console.log(all_cities_and_countries);
+
+
+// const myArr = [1,2,3,[4,5],6,7,[8,9,[3,4]]]
+// const myArr2 = myArr.flat(Infinity)
+// console.log(myArr2);
+
+
+
+console.log(Array.isArray("Salman"));
+console.log(Array.from("Salman"));
+
