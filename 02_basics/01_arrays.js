11977519a001366f4e702401553399320ae6d669
salman-khan-530 <salman22000055@gmail.com>
2026-09-13 22:27:40 +0500

    Array in js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,49 @@
+// Array
+
+const myArr = [1,2,3,4,5,6]
+const myCities = ["Mardan", "Charsadda", "Swabi"]
+
+const myArr2 = new Array(1,2,3,4,5)
+
+// console.log(myArr[4]);
+
+
+// array methods
+
+// myArr.push(7)
+// myArr.push(8)
+// myArr.pop()
+// console.log(myArr);
+
+
+// myArr.unshift(3)
+// myArr.shift()
+// console.log(myArr);
+
+
+// console.log(myArr.includes(9));
+// console.log((myArr.indexOf(3)));
+
+
+// const newArr = myArr.join() // join convert array into string
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2);
+console.log("C ", myArr);
+
+
+
